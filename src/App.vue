64d1d4cbfb6896a5849cb4e2cda1a23395b7b13f
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-02-09 10:54:01 +0530

    created top nab bar

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@ import NavBar from './components/layout/NavBar.vue'
 
 <template>
   <NavBar />
+
+
   <!-- Put real content here -->
-  <div class="bg-red-200">
-    <h1 class="text-xl text-blue-600 font-bold underline">
+  <!-- <div class="bg-primary text-secondary mt-36">
+    <h1 class="text-xl text-blue-100 font-bold underline">
       Welcome to your new Vue + Tailwind project!
     </h1>
     <p>Now it should be visible1 😄</p>
-  </div>
+  </div> -->
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite';
-import Icons from 'unplugin-icons/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-     tailwindcss(),
-         Icons({
+  plugins: [
+    vue(),
+    Icons({
       compiler: 'vue3',
       autoInstall: true,
     }),
   ],
-
-})
+});
