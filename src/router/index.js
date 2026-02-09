60e3e0fee2ff2d89c301f678ed44a42a7d13d6e8
NIPUNASUDESH <nipunasudesh2000@gmail.com>
2026-02-09 19:03:31 +0530

    created thankyou page

--- src/router/index.js
+++ src/router/index.js
@@ -3,39 +3,42 @@ import PublicLayout from "../components/layout/PublicLayout.vue";
 import Banner from "../views/home/Banner.vue";
 import index from "../views/home/index.vue";
 import About from "../views/About.vue";
+import PrivacyPolicy from "../views/PrivacyPolicy.vue";
+import Terms from "../views/Terms.vue";
+import ThankYou from "../views/ThankYou.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Contact from "../views/Contact.vue";
 
 const routes = [
-  {
+    {
     path: "/",
     component: PublicLayout,
     children: [
-      { path: "", component: index }, 
+      { path: "", name: "Home", component: index },
+      { path: "about", name: "About", component: About },
+      { path: "contact", name: "Contact", component: Contact },
+      { path: "login", name: "Login", component: Login },
+      { path: "register", name: "Register", component: Register },
+      { path: "thank-you", name: "ThankYou", component: ThankYou },
+      { path: "privacy-policy", name: "PrivacyPolicy", component: PrivacyPolicy },
+      { path: "terms", name: "Terms", component: Terms },
+
+      { path: "all-products", name: "AllProducts", component: Banner },
+      { path: "all-categories", name: "AllCategories", component: Banner },
+      {
+        path: "product-category/:category",
+        name: "ProductCategory",
+        component: Banner,
+        props: true,
+      },
     ],
-  },
-    {
-    path: '/all-products',
-    name: 'AllProducts',
-    component: Banner,
-  },
-    {
-    path: "/about",
-    component: PublicLayout,
-    children: [
-      { path: "", component: About }, 
-    ],
-  },
-  {
-    path: '/product-category/:category',
-    name: 'ProductCategory',
-    component: Banner,
   },
     { path: "/profile", component: Banner },
   { path: "/all-categories", component: Banner },
-  { path: "/contact", component: Banner },
-  { path: "/privacy-policy", component: Banner },
-  { path: "/terms", component: Banner },
+
   // add 404 later
-  // { path: "/:pathMatch(.*)*", component: () => import("../views/ErrorPage.vue") },
+   { path: "/:pathMatch(.*)*", component: () => import("../views/ErrorPage.vue") },
 ];
 
 const router = createRouter({
